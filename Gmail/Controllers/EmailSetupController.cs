ccaac921e810500b8c488a85fb0bc4785b652980
hafizalpantho <hafizalpantho@gmail.com>
2021-03-29 23:11:21 +0600

    first commit

---
+++ Gmail/Controllers/EmailSetupController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Gmail.Models;
+using System.Net;
+using System.Net.Mail;
+
+
+
+namespace Gmail.Controllers
+{
+    public class EmailSetupController : Controller
+    {
+        // GET: EmailSetup
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Gmail.Models.gmail model)
+        {
+            MailMessage mm = new MailMessage("hafizalpantho@gmail.com", model.To);
+            mm.Subject = model.Subject;
+            mm.Body = model.Body;
+            mm.IsBodyHtml = false;
+            
+
+
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+
+            NetworkCredential nc = new NetworkCredential("hafizalpantho@gmail.com","EnterYourPassword");
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = nc;
+            smtp.Send(mm);
+            ViewBag.Message = "Mail has been sent successfully!";
+            return View();
+        }
+    }
+}
